daf85c76271c7d8deac6ad16158614b68b57a1de
Lu Gan <ganlu@umich.edu>
2023-06-21 03:37:29 -0700

    formatting

--- train_m3fd_rgb_cls.bash
+++ train_m3fd_rgb_cls.bash
@@ -1,2 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-python train.py root='' --bench-task=train_cls --dataset=m3fd_rgb --model=efficientdetv2_dt --batch-size=16 --amp --lr=1e-4 --opt adam --sched plateau --num-classes=6 --mean 0.49151019 0.50717567 0.50293698 --std 0.1623529 0.14178433 0.13799928 --save-images --workers=4 --initial-checkpoint /home/ganlu/workspace/efficientdet-pytorch/output/train/20230524-003411-efficientdetv2_dt/model_best.pth.tar
+python train.py root='' --eval-metric=acc --bench-task=train_cls --dataset=m3fd_rgb --model=efficientdetv2_dt \
+--batch-size=16 --amp --lr=1e-4 --opt adam --sched plateau --num-classes=6 \
+--mean 0.49151019 0.50717567 0.50293698 --std 0.1623529 0.14178433 0.13799928 \
+--save-images --workers=4 --initial-checkpoint /home/ganlu/workspace/efficientdet-pytorch/output/train/20230524-003411-efficientdetv2_dt/model_best.pth.tar
