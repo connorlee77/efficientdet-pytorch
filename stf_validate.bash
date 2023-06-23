f1daacd26fd65a417c33464e970d0c823647e446
Lu Gan <ganlu@umich.edu>
2023-06-23 13:51:40 -0700

    minor change

--- stf_validate.bash
+++ stf_validate.bash
@@ -5,6 +5,6 @@ export CUDA_VISIBLE_DEVICES=0
 
 
 
-python validate.py root='' --dataset=stf_snow_rain_rgb --model efficientdetv2_dt --workers 16 --checkpoint stf_1280_clear_weights/20230622-162846-efficientdetv2_dt/model_best.pth.tar --num-classes=4 -b 16 --results rgb_test_clear_easy.txt --split test --img-size 1280
+python validate.py root='' --dataset=stf_snow_rain_rgb --model efficientdetv2_dt --workers 16 --checkpoint output/stf_1280_clear_weights/20230622-162846-efficientdetv2_dt/model_best.pth.tar --num-classes=4 -b 16 --results rgb_test_clear_easy.txt --split test --img-size 1280
 
-python validate.py root='' --dataset=stf_snow_rain_gated --model efficientdetv2_dt --workers 16 --checkpoint stf_1280_clear_weights/20230622-162902-efficientdetv2_dt/model_best.pth.tar --num-classes=4 -b 16 --results rgb_test_clear_easy.txt --split test --img-size 1280
+python validate.py root='' --dataset=stf_snow_rain_gated --model efficientdetv2_dt --workers 16 --checkpoint output/stf_1280_clear_weights/20230622-162902-efficientdetv2_dt/model_best.pth.tar --num-classes=4 -b 16 --results rgb_test_clear_easy.txt --split test --img-size 1280
